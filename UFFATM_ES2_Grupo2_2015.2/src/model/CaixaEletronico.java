755f23a05a38772676045aa1921f096cbcb60cb2
ccz <ccz@ccz-VirtualBox>
2016-01-21 23:27:33 -0200

    Criação de classes básicas

---
+++ UFFATM_ES2_Grupo2_2015.2/src/model/CaixaEletronico.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ccz
+ */
+public class CaixaEletronico {
+    
+    private int papel;
+    private int cheque;
+    
+    private int nota10;
+    private int nota20;
+    private int nota50;
+    private int nota100;
+    
+}
---
+++ UFFATM_ES2_Grupo2_2015.2/src/model/Transacao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ccz
+ */
+public abstract class Transacao {
+    private Date dataAtual; //tipo não definitivo
+    
+}
