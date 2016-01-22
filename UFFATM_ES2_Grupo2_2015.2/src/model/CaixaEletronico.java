07354c47489ad02d3c04c3ca2a8d6a41c8731438
Augusto Sales <augustocezar@id.uff.br>
2016-01-21 23:51:13 -0200

    Criação de classes básicas

--- UFFATM_ES2_Grupo2_2015.2/src/model/CaixaEletronico.java
+++ UFFATM_ES2_Grupo2_2015.2/src/model/CaixaEletronico.java
@@ -18,5 +18,14 @@ public class CaixaEletronico {
     private int nota20;
     private int nota50;
     private int nota100;
+
+    public CaixaEletronico(int papel, int cheque, int nota10, int nota20, int nota50, int nota100) {
+        this.papel = papel;
+        this.cheque = cheque;
+        this.nota10 = nota10;
+        this.nota20 = nota20;
+        this.nota50 = nota50;
+        this.nota100 = nota100;
+    }
     
 }
